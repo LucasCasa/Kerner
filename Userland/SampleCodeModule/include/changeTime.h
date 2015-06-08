81654a93cf421c647b1379456a2af6e9af310f53
maggievega <maggievega@hotmail.com>
2015-06-08 15:01:16 -0300

    Ahora no puede borrar cuando pone change time

--- Userland/SampleCodeModule/include/changeTime.h
+++ Userland/SampleCodeModule/include/changeTime.h
@@ -15,12 +15,12 @@ int validateDay(int day, int month, int year);
 
 int MAX_VALUES[]={9999,12,31,23,59,59};
 int MIN_VALUES[]={0,1,1,0,0,0};
-char INPUT_YEAR[]="Ingrese nuevo Anio: ";
-char INPUT_MONTH[]="Ingrese nuevo Mes: ";
-char INPUT_DAY[]="Ingrese nuevo Dia: ";
-char INPUT_HOUR[]="Ingrese nueva Hora: ";
-char INPUT_MIN[]="Ingrese nuevo Minuto: ";
-char INPUT_SEC[]="Ingrese nuevo Segundo: ";
+char INPUT_YEAR[]="Ingrese nuevo Anio:\n";
+char INPUT_MONTH[]="Ingrese nuevo Mes:\n";
+char INPUT_DAY[]="Ingrese nuevo Dia:\n";
+char INPUT_HOUR[]="Ingrese nueva Hora:\n";
+char INPUT_MIN[]="Ingrese nuevo Minuto:\n";
+char INPUT_SEC[]="Ingrese nuevo Segundo:\n";
 char * INPUT[]={INPUT_YEAR,INPUT_MONTH,INPUT_DAY,INPUT_HOUR,INPUT_MIN,INPUT_SEC};
 
 
